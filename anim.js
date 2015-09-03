ad840756fb115d538aef0d8a4096eabca2a3d7a3
Mohamed Hayibor <mhayibor@suffolk.edu>
2015-09-03 14:38:32 -0400

    first

---
+++ anim.js
@@ -0,0 +1,28 @@
+
+setInterval(function () {
+	$('#one').toggleClass('magictime bombLeftOut');
+}, 3000);
+
+setInterval(function() {
+	$('#two').toggleClass('magictime spaceInUp');
+}, 5000);
+
+setInterval(function() {
+	$('#three').toggleClass('magictime bombRightOut');
+}, 3000);
+
+setInterval(function() {
+	$('#four').toggleClass('magictime perspectiveLeft');
+}, 7000);
+
+setInterval(function() {
+	$('#five').toggleClass('magictime foolishIn');
+}, 10000);
+
+setInterval(function() {
+	$('#six').toggleClass('magictime perspectiveRight');
+}, 7000);
+
+
+
+
